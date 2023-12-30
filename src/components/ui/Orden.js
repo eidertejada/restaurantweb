6ecb57b4537b034284a91e534e11df6164d6ed5e
Eider <eiderguillermo.et@gmail.com>
2023-12-29 21:22:48 -0400

    Arreglo de usuarios

--- src/components/ui/Orden.js
+++ src/components/ui/Orden.js
@@ -64,7 +64,9 @@ const Orden = ({ orden }) => {
   return (
     <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
       <div className="p-3 shadow-md bg-white">
-        <h1 className="text-yellow-600 text-lg font-bold">{orden.id}</h1>
+        <h1 className="text-yellow-600 text-lg font-bold">
+          {orden.usuario ? `Usuario: ${orden.usuario}` : orden.id}
+        </h1>
         {orden.orden.map((platillos) => (
           <p className="text-gray-600">
             {platillos.cantidad} {platillos.nombre}
